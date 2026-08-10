8437e47869b6777439722fe7557312921e9db65a
ridhima09-boop <ridhimasuneja9@gmail.com>
2026-08-11 01:09:53 +0530

    Implement merging of two arrays into a third array

---
+++ Q3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int a[50], b[50], c[100];
+    int n, m, i = 0, j = 0, k = 0;
+
+    printf("Enter size of first array: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    printf("Enter size of second array: ");
+    scanf("%d", &m);
+
+    for (i = 0; i < m; i++)
+        scanf("%d", &b[i]);
+
+    i = j = 0;
+
+    while (i < n && j < m) {
+        if (a[i] < b[j])
+            c[k++] = a[i++];
+        else
+            c[k++] = b[j++];
+    }
+
+    while (i < n)
+        c[k++] = a[i++];
+
+    while (j < m)
+        c[k++] = b[j++];
+
+    printf("Merged array: ");
+    for (i = 0; i < k; i++)
+        printf("%d ", c[i]);
+
+    return 0;
+}
